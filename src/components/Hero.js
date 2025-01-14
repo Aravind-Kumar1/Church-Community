6a50c4f8eb651e0a10e1abbd0918bf0aab361168
vittypaul <chamaakuriaravind@gmail.com>
2025-01-14 07:53:21 +0530

    Your update message

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,22 @@
 import Link from 'next/link';
 import styles from '../styles/Hero.module.css';
-import '../styles/global.css'; // Do not use `styles` as the alias for global CSS.
-
+import '../styles/global.css';
 
 const Hero = () => {
   return (
     <section className={styles.heroSection}>
       <div className={styles.heroContent}>
         <h1 className={styles.heroTitle}>
-          Building a <span className={styles.bold}>Community</span><br />
-          of <span className={styles.bold}>Grace</span> and <span className={styles.bold}>Hope</span>
+          Building a <span className={styles.bold}>Community</span>
+          <br />
+          of <span className={styles.bold}>Grace</span> and{' '}
+          <span className={styles.bold}>Hope</span>
         </h1>
-
         <p className={styles.heroSubtitle}>
           “Let the message of Christ dwell among you richly as you teach and admonish one another with all wisdom” - Colossians 3:16
         </p>
         <p className={styles.heroDescription}>
-         Join a vibrant community rooted in love, faith, and service. Together, let’s worship, grow, and make a difference with purpose.
+          Join a vibrant community rooted in love, faith, and service. Together, let’s worship, grow, and make a difference with purpose.
         </p>
         <div className={styles.heroButtons}>
           <Link href="/up-events" className={styles.button}>
@@ -27,11 +27,13 @@ const Hero = () => {
           </Link>
         </div>
       </div>
-      <img
-        src="/emmanuel church.jpg" /* Replace with your actual image path */
-        alt="Church Community"
-        className={styles.heroImage}
-      />
+      <div className={styles.heroImageWrapper}>
+        <img
+          src="/emmanuel church.jpg"
+          alt="Church Community"
+          className={styles.heroImage}
+        />
+      </div>
     </section>
   );
 };
